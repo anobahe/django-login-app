3dbfd17fabbf04b6422736d5525d33e18a98cc38
anobahe <95242751+anobahe@users.noreply.github.com>
2024-07-22 07:12:37 +0000

    Update info.py

--- loginproject/loginproject/info.py
+++ loginproject/loginproject/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'jenniferseyramamedior@gmail.com'
-EMAIL_HOST_PASSWORD = 'hi0275050287'
+EMAIL_HOST_USER = '*******@gmail.com'
+EMAIL_HOST_PASSWORD = '*******'
 EMAIL_PORT = 587
